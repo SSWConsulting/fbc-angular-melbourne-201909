37daf9e99aaea907bab5feb0e641232cc7a2ebce
brendan richards <brendan_richards_msdn@hotmail.com>
2019-09-12 13:09:23 +1000

    completed CompanyService

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Company } from './company';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -25,6 +25,27 @@ export class CompanyService {
   handleError(e): Observable<any> {
     console.error('ERROR! OH NO!', e);
     return of({});
+  }
+
+  deleteCompany(company: Company) {
+    console.log('Deleting Company');
+    return this.httpClient.delete<Company>(`${this.API_BASE}/company/${company.id}`);
+  }
+
+  addCompany(company: Company): Observable<Company>{
+    return this.httpClient.post<Company>(`${this.API_BASE}/company`, company
+    , { headers: new HttpHeaders().set('content-type', 'application/json') });
+  }
+
+  getCompany(id: number): Observable<Company> {
+    return this.httpClient.get<Company>(`${this.API_BASE}/company/${id}`);
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this.httpClient.put<Company>(
+      `${this.API_BASE}/company/${company.id}`, company,
+      { headers: new HttpHeaders().set('content-type', 'application/json') }
+    );
   }
 
 
