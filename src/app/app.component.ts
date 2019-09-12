06e6742386c0ff3830f95352637ff73868e3755f
brendan richards <brendan_richards_msdn@hotmail.com>
2019-09-12 10:44:43 +1000

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebootcamp-crm';
+  city = 'Melbourne';
+  date = new Date();
 }
